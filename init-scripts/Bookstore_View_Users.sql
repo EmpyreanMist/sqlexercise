5adb6c5b9e11f3751e4c44fca185c64e1686ad7b
Christian <christianfryksten1@gmail.com>
2025-10-22 11:46:11 +0200

    Allow users to connect through docker

--- init-scripts/Bookstore_View_Users.sql
+++ init-scripts/Bookstore_View_Users.sql
@@ -13,17 +13,39 @@ GROUP BY a.id, a.first_name, a.last_name, a.birth_date;
 
 SELECT * FROM total_author_book_value;
 
+-- Lokalt konto
 CREATE USER IF NOT EXISTS 'developer'@'localhost' IDENTIFIED BY 'devpassword';
+-- Fjärråtkomst (för Docker/verktyg som inte ansluter som localhost)
+CREATE USER IF NOT EXISTS 'developer'@'%' IDENTIFIED BY 'devpassword';
+
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX
     ON bookstore.* TO 'developer'@'localhost';
-REVOKE CREATE USER, CREATE, GRANT OPTION
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX
+    ON bookstore.* TO 'developer'@'%';
+
+-- Säkerhet – ta bort administrativa rättigheter
+REVOKE CREATE USER, GRANT OPTION
     ON *.* FROM 'developer'@'localhost';
+REVOKE CREATE USER, GRANT OPTION
+    ON *.* FROM 'developer'@'%';
 
 CREATE USER IF NOT EXISTS 'webserver'@'localhost' IDENTIFIED BY 'webpassword';
+CREATE USER IF NOT EXISTS 'webserver'@'%' IDENTIFIED BY 'webpassword';
+
 GRANT SELECT, INSERT, UPDATE, DELETE
     ON bookstore.* TO 'webserver'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE
+    ON bookstore.* TO 'webserver'@'%';
+
+-- Säkerhet – ta bort schema- och adminrättigheter
 REVOKE CREATE, DROP, ALTER, CREATE USER, GRANT OPTION
     ON *.* FROM 'webserver'@'localhost';
+REVOKE CREATE, DROP, ALTER, CREATE USER, GRANT OPTION
+    ON *.* FROM 'webserver'@'%';
+
+FLUSH PRIVILEGES;
 
 SHOW GRANTS FOR 'developer'@'localhost';
+SHOW GRANTS FOR 'developer'@'%';
 SHOW GRANTS FOR 'webserver'@'localhost';
+SHOW GRANTS FOR 'webserver'@'%';
